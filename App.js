3b61c9888af06f8c667ef8276cda793181bd2aed
Huy <vohuy060214@gmail.com>
2024-08-11 19:14:11 +0700

    Event input

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+
+  const [todoText, setTodoText] = useState('');
+
+  function textInputChange(textChanged) {
+    setTodoText(textChanged);
+  }
+  function addToDo() {
+    console.log(todoText);
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Todo List'
+          style={styles.textInPut}
+          onChangeText={textInputChange}
+        ></TextInput>
+        <Button title='Add' onPress={addToDo}></Button>
+      </View>
+      <View style={styles.todoList}>
+        <Text>To do list</Text>
+      </View>
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    paddingTop: 60,
+    flex: 1
   },
-});
+
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 50,
+    borderBottomColor: 'blue',
+    borderBottomWidth: 2,
+    paddingBottom: 30,
+    flex: 1,
+    alignItems: 'center'
+  },
+  textInPut: {
+    borderWidth: 2,
+    borderColor: 'green',
+    width: '85%',
+    marginRight: 10,
+    padding: 3.5
+  },
+  todoList: {
+    flex: 6
+  }
+})
+
+
